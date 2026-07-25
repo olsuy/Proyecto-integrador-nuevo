84e7464f2ff9f683390ec560194cac1f72168160
Ozzy <olmedoosmar3@gmail.com>
2026-07-25 01:22:39 -0600

    pequeña modificacion al listen

--- pruebas2/conexion/Server.js
+++ pruebas2/conexion/Server.js
@@ -59,7 +59,7 @@ app.post("/api/login", async (req, res) => {
   }
 });
 
-const PORT = Number(process.env.PORT) || 5000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
