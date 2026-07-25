ee3fbcf7bc87c0eeb4da47038582f18ad347aba2
Ozzy <olmedoosmar3@gmail.com>
2026-07-24 20:42:47 -0600

    pequeño cambio en el servidor

--- pruebas2/conexion/Server.js
+++ pruebas2/conexion/Server.js
@@ -44,6 +44,8 @@ app.post("/api/login", async (req, res) => {
   }
 });
 
-app.listen(5000, "127.0.0.1", () => {
-  console.log("Servidor corriendo en http://127.0.0.1:5000");
+const PORT = Number(process.env.PORT) || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
