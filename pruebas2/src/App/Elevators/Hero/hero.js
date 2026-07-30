33f42ab153036fe2284f2cbe2318ad6115e1610d
Ozzy <olmedoosmar3@gmail.com>
2026-07-30 17:19:15 -0600

    correcciones a diseño mal estructurado

--- pruebas2/src/App/Elevators/Hero/hero.js
+++ pruebas2/src/App/Elevators/Hero/hero.js
@@ -5,16 +5,10 @@ import elevator from "../elevator.png";
 import Status_cards from "../Status_cards/st_cards.js"
 import PanelInfo from "../panel/panel.js";
 
-
 function Hero() {
   return (
-    <section
-    
-      className="hero"
-      style={{ backgroundImage: `url(${fondo})` }}
-    >
-    
-       
+    <section className="hero" style={{ backgroundImage: `url(${fondo})` }}>
+      
       <div className="hero-overlay">
         
         <div className="hleft">
@@ -33,71 +27,60 @@ function Hero() {
             elevators, collecting and visualizing key data to improve
             efficiency, safety and operational control.
           </p>
-
         </div>
 
-       <div className="hright">
-
-        <div className="panel">
-
+        <div className="hright">
+          <div className="panel">
             <h3>ELEVATOR STATUS</h3>
+            
             <img
-            src={elevator}
-            alt="Elevator"
-            className="elevatorimg"
-               />
+              src={elevator}
+              alt="Elevator"
+              className="elevatorimg"
+            />
 
             <div className="status">
-
-                <span className="status_elevator"></span>
-
-                <p>RUNNING</p>
-
+              <span className="status_elevator"></span>
+              <p>RUNNING</p>
             </div>
+            
             <PanelInfo
-             className="floor"
-             title="Current Floor"
-             value="5"
+              className="door"
+              title="Door Status"
+              value="CLOSED"
             />
-
-
-           <PanelInfo
-           className="direction"
-           title="Direction"
-           value="UP"
-            />
-
 
             <PanelInfo
-           className="door"
-           title="Door Status"
-           value="CLOSED"
+              className="floor"
+              title="Current Floor"
+              value="5"
             />
 
-
-           <PanelInfo
-            className="speed"
-            title="Speed"
-             value="1.2 m/s"
+            <PanelInfo
+              className="direction"
+              title="Direction"
+              value="UP"
             />
 
+            <PanelInfo
+              className="speed"
+              title="Speed"
+              value="1.2 m/s"
+            />
 
-           <PanelInfo
-            className="update"
-             title="Last Update"
-             value="10:24 AM"
-             />
-
-
-
-
+            <PanelInfo
+              className="update"
+              title="Last Update"
+              value="10:24 AM"
+            />
+          </div>
         </div>
-   
-       </div>
 
       </div>
       
+      {/* Las tarjetas ahora fluirán de manera natural debajo del contenido gracias al CSS ajustado */}
       <Status_cards/>   
+      
     </section>
   );
 }
